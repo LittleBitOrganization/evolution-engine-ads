6884a02ada03bda8e99873b96aafcba9de49017b
TerentevVlad <ter.vlad.ter@gmail.com>
2022-08-30 12:10:00 +0300

    Добавил команды для показа рекламы

---
+++ Runtime/Command/CommandAds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LittleBitGames.Environment.Ads;
+
+namespace LittleBitGames.Ads
+{
+    public class CommandAds
+    {
+        private readonly IAdsService _adsService;
+        private readonly AdType _type;
+        private readonly IAdUnitPlace _place;
+        private readonly SkipAdsCondition _skipAdsCondition;
+
+        public CommandAds(IAdsService adsService, AdType type, IAdUnitPlace place, SkipAdsCondition skipAdsCondition)
+        {
+            _adsService = adsService;
+            _type = type;
+            _place = place;
+            _skipAdsCondition = skipAdsCondition;
+        }
+
+        public void Execute(Action<bool> onShowed)
+        {
+            if (_skipAdsCondition.Invoke())
+            {
+                onShowed?.Invoke(true);
+            }
+            else
+            {
+                _adsService.ShowAd(_type, _place, info => { onShowed?.Invoke(info.Success); });
+            }
+        }
+    }
+}
---
+++ Runtime/Factories/CommandAdsFactory.cs
@@ -0,0 +1,25 @@
+﻿using LittleBitGames.Environment.Ads;
+
+namespace LittleBitGames.Ads
+{
+    public class CommandAdsFactory
+    {
+        private readonly IAdsService _adsService;
+        private SkipAdsCondition _skipAdsCondition;
+
+        public CommandAdsFactory(IAdsService adsService)
+        {
+            _adsService = adsService;
+        }
+
+        public CommandAds ShowReward(IAdUnitPlace place)
+        {
+            return new CommandAds(_adsService, AdType.Rewarded, place, _skipAdsCondition);
+        }
+
+        public CommandAds ShowInter(IAdUnitPlace place)
+        {
+            return new CommandAds(_adsService, AdType.Inter, place, _skipAdsCondition);
+        }
+    }
+}
